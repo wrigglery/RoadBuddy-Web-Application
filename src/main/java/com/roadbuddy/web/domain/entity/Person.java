f9051bd8581f6cbe5c95563a17e73bee2bf698bb
morozovcookie <morozov.cookie@gmail.com>
2017-04-14 21:56:43 +0300

    [ref] Подогнал класс настроек под текущий файл свойств, добавил правила сборки для Travis CI

--- src/main/java/com/roadbuddy/web/domain/entity/Person.java
+++ src/main/java/com/roadbuddy/web/domain/entity/Person.java
@@ -1,6 +1,9 @@
 package com.roadbuddy.web.domain.entity;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
 @Entity
 public class Person {
--- src/main/java/com/roadbuddy/web/settings/SpringSettings.java
+++ src/main/java/com/roadbuddy/web/settings/SpringSettings.java
@@ -9,31 +9,7 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties("spring")
 public class SpringSettings {
 
-    public void setProfiles(String profiles) {
-        this.profiles = profiles;
-    }
-
-    public String getProfiles() {
-        return profiles;
-    }
-
-    public void setData(Data data) {
-        this.data = data;
-    }
-
-    public Data getData() {
-        return data;
-    }
-
     public static class Data {
-
-        public void setRest(Rest rest) {
-            this.rest = rest;
-        }
-
-        public Rest getRest() {
-            return rest;
-        }
 
         public static class Rest {
 
@@ -48,10 +24,123 @@ public class SpringSettings {
             private String basePath;
         }
 
+        public void setRest(Rest rest) {
+            this.rest = rest;
+        }
+
+        public Rest getRest() {
+            return rest;
+        }
+
         private Rest rest;
     }
 
-    private String profiles;
+    public void setData(Data data) {
+        this.data = data;
+    }
+
+    public Data getData() {
+        return this.data;
+    }
+
+    public static class DataSource {
+
+        public void setDriverClassName(String driverClassName) {
+            this.driverClassName = driverClassName;
+        }
+
+        public String getDriverClassName() {
+            return driverClassName;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUserName(String userName) {
+            this.userName = userName;
+        }
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        private String driverClassName;
+
+        private String url;
+
+        private String userName;
+
+        private String password;
+    }
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public DataSource getDataSource() {
+        return this.dataSource;
+    }
+
+    public static class Jpa {
+
+        public static class Hibernate {
+
+            public void setDdlAuto(String ddlAuto) {
+                this.ddlAuto = ddlAuto;
+            }
+
+            public String getDdlAuto() {
+                return ddlAuto;
+            }
+
+            private String ddlAuto;
+        }
+
+        public void setHibernate(Hibernate hibernate) {
+            this.hibernate = hibernate;
+        }
+
+        public Hibernate getHibernate() {
+            return hibernate;
+        }
+
+        public void setShowSql(boolean showSql) {
+            this.showSql = showSql;
+        }
+
+        public boolean isShowSql() {
+            return showSql;
+        }
+
+        private Hibernate hibernate;
+
+        private boolean showSql;
+    }
+
+    public void setJpa(Jpa jpa) {
+        this.jpa = jpa;
+    }
+
+    public Jpa getJpa() {
+        return jpa;
+    }
 
     private Data data;
+
+    private DataSource dataSource;
+
+    private Jpa jpa;
 }
